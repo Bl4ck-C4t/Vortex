4ffb28a8329d7887a409fa857bbb36726d4274a9
TheShadow88 <stef4oben88@gmail.com>
2020-03-12 17:04:45 +0200

    [feat] Improved the lexer-only version

--- debugMain.cc
+++ debugMain.cc
@@ -9,10 +9,60 @@ int main(){
     while(true){
         std::cout << "Scanning: ";
         yy::parser::symbol_type ret = yylex(drv);
-        if(ret.type == 0){
-            break;
+        switch(ret.type){
+            case 0:
+                exit(0);
+            case 3:
+                std::cout << "TYPE" << std::endl;
+                break;
+            case 4:
+                std::cout << "SYMBOL: " << ret.value.as<std::string>() << std::endl;
+                break;
+
+            case 28:
+                std::cout << "{" << std::endl;
+                break;
+
+            case 29:
+                std::cout << "}" << std::endl;
+                break;
+
+            case 30:
+                std::cout << ";" << std::endl;
+                break;
+
+            case 40:
+                std::cout << "class" << std::endl;
+                break;
+
+            case 32:
+                std::cout << "fn" << std::endl;
+                break;
+            
+            case 24:
+                std::cout << "(" << std::endl;
+                break;
+
+            case 25:
+                std::cout << ")" << std::endl;
+                break;
+            
+            case 33:
+                std::cout << "->" << std::endl;
+                break;
+
+            case 23:
+                std::cout << "," << std::endl;
+                break;
+            
+            case 6:
+                std::cout << "BODY" << std::endl;
+                break;
+
+            default:
+                std::cout << ret.type << std::endl;
+
         }
-        std::cout << ret.type << " " << ret.value.as<std::string>() << std::endl;
 
     }
     return 0;
